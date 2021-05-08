8570249ba889ab31f90518bd346f55af4ca14b18
Mikhail <mikepautov@gmail.com>
2021-05-08 23:29:03 +0300

    dataset

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,7 +1,25 @@
 import torch
-class RandomGeneratedSequences(torch.utils.data.Dataset):
-    pass
+import os
+import numpy as np
+import pandas as pd
 
-dataset_dict = {
-    'sin_k': RandomGeneratedSequences,
-}
+class RandomGeneratedSequences(torch.utils.data.Dataset):
+    def __init__(self, path_to_csvs):
+        self.path = path_to_csvs
+        
+    def __len__(self):
+        files = os.listdir(self.path)
+        if 'clusters.csv' in files:
+            files.remove('clusters.csv')
+        return sum([file.endswith('.csv') for file in files])
+    
+    def __getitem__(self, index):
+        files = os.listdir(self.path)
+        target = None #
+        if 'clusters.csv' in files:
+            target = torch.Tensor(pd.read_csv(self.path + '/clusters.csv')['cluster_id'])[index]
+            files.remove('clusters.csv')
+        data_mask = [file.endswith('.csv') for file in files]
+        data = np.array(files)[data_mask].tolist()
+        sample = torch.Tensor(np.array(pd.read_csv(self.path + data[index])))
+        return sample, target
