d47a2b27c9bdddf8284db88289421ff219080dd4
khakhulin <t.khakhulin@gmail.com>
2021-05-10 21:43:37 +0300

    fixing simple dataset

--- src/dataset/__init__.py
+++ src/dataset/__init__.py
@@ -1,47 +1,52 @@
-# +
-import torch
 import os
 import numpy as np
 import pandas as pd
+import torch
+from torch.utils.data import Dataset, DataLoader
 
-class RandomGeneratedSequences(torch.utils.data.Dataset):
-    def __init__(self, path_to_csvs):
+
+class RandomGeneratedSequences(Dataset):
+    def __init__(self, path_to_csvs, num_of_event_types,
+                 num_of_steps=-1, predefined_step_size=False):
         self.path = path_to_csvs
-        
-    def __len__(self):
-        files = os.listdir(self.path)
-        if 'clusters.csv' in files:
-            files.remove('clusters.csv')
-        return sum([file.endswith('.csv') for file in files])
-    
-    def __getitem__(self, index):
-        files = os.listdir(self.path)
-        target = None #
-        if 'clusters.csv' in files:
-            target = torch.Tensor(pd.read_csv(self.path + '/clusters.csv')['cluster_id'])[index]
-            files.remove('clusters.csv')
+        files = list(filter(lambda x: x != 'clusters.csv', os.listdir(self.path)))
         data_mask = [file.endswith('.csv') for file in files]
-        data = np.array(files)[data_mask].tolist()
-        sample = torch.tensor(np.array(pd.read_csv(self.path + data[index])))
-        return sample, target
-    
-    
+        self.targets = torch.from_numpy(np.array(pd.read_csv(self.path + '/clusters.csv')['cluster_id']))
+        self.file_paths = np.array(files)[data_mask].tolist()
+        self.num_of_steps = num_of_steps
+        self.num_of_event_types = num_of_event_types + 1
+        self.predefined_step_size = predefined_step_size
+        self.partitions = torch.zeros(len(self.file_paths), num_of_steps, self.num_of_event_types)
+        for i, file_path in enumerate(self.file_paths):
+            sample = np.array(pd.read_csv(os.path.join(self.path, self.file_paths[i])))
+            sample_df = pd.DataFrame(sample, columns=['idx', 'time', 'event'])
+            sample_df = sample_df.drop(columns='idx')
+            partitions = get_partition(sample_df, self.num_of_steps, self.num_of_event_types - 1)
+            self.partitions[i] = partitions
+
+    def __len__(self):
+        return len(self.file_paths)
+
+    def __getitem__(self, index):
+        return self.partitions[index], self.targets[index]
+
+
 def get_partition(sample, num_of_steps, num_of_event_types, end_time=None):
-    '''
-        Transforms a sample into partition
-        inputs:
-                sample -  pd.DataFrame; columns - time and type of event, sorted by time
-                num_of_steps - int, number of steps in partition
-                num_of_event_type - int, number of event types
-                end_time - float, end time or None
-        outputs:
-                partition - torch.tensor, size = (num_of_steps, num_of_classes + 1)
-    '''
+    """
+    Transforms a sample into partition
+    inputs:
+            sample -  pd.DataFrame; columns - time and type of event, sorted by time
+            num_of_steps - int, number of steps in partition
+            num_of_event_type - int, number of event types
+            end_time - float, end time or None
+    outputs:
+            partition - torch.tensor, size = (num_of_steps, num_of_classes + 1)
+    """
     if end_time is None:
         end_time = sample['time'][len(sample['time']) - 1]
-        
+
     partition = torch.zeros(num_of_steps, num_of_event_types + 1)
-    
+
     # finding time stamp
     dt = end_time / num_of_steps
     partition[:, 0] = end_time / num_of_steps
@@ -69,19 +74,11 @@ def get_partition(sample, num_of_steps, num_of_event_types, end_time=None):
         sample['event'] = sample['event'].astype(int)
 
     # computing partition
-    temp = torch.Tensor(sample.to_numpy()).long()
+    temp = torch.from_numpy(sample.to_numpy())
     partition[temp[:, 0], temp[:, 1] + 1] = temp[:, 2].float()
     return partition
 
 
-
-def batch_to_partitions(batch, num_of_steps, num_of_event_types, end_time=None):
-    '''
-    The function produces partitions for batch of data of RGS dataset
-    '''
-    partitions = torch.zeros(len(batch), num_of_steps, num_of_event_types + 1)
-    for num_of_sample, sample in enumerate(batch):
-        sample_df = pd.DataFrame(sample.numpy(), columns=['idx', 'time', 'event'])
-        sample_df = sample_df.drop(columns='idx')
-        partitions[num_of_sample] = get_partition(sample_df, num_of_steps, num_of_event_types)
-    return partitions
+if __name__ == '__main__':
+    dataset_ex = RandomGeneratedSequences('data/sin_K2_C5', num_of_steps=1000, num_of_event_types=8)
+    partitions = []
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,3 +1,9 @@
 from src.utils.base import *
 from src.utils.metrics import *
 from src.utils.net import *
+
+
+def list_collate_fn(batch):
+    x = [item[0] for item in batch]
+    y = [item[1] for item in batch]
+    return x, y
