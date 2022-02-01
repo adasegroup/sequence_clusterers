cca69b934ddd696e503616b5ddcfa19eee0f5ba9
Seva Grabar <grabarseva@gmail.com>
2022-02-01 00:16:50 +0000

    refactor run,py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-from src.model.train_model import train_model
-from src.model.inference_only import run_inference
-from src.model.thp_optimize import thp_optuna
+from src.train_model import train_model
+from src.inference_only import run_inference
+from src.thp_optimize import thp_optuna
